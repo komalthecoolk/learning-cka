5cd491bdb0a93aef8e85023699b4c8aadb255379
komalthecoolk <>
2025-12-28 18:48:28 +0530

    gke deployment code

--- 01- GCP GKE Cluster/main.tf
+++ 01- GCP GKE Cluster/main.tf
@@ -1,28 +1,83 @@
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
-}
-
 resource "google_container_cluster" "primary" {
   name               = var.cluster_name
-  location           = var.zone
-  initial_node_count = 1
+  location           = var.region
+  initial_node_count = var.num_nodes
+  node_locations     = var.node_locations
 
+  # Cluster version and release channel
+  cluster_autoscaling {
+    enabled = false
+  }
+
+  release_channel {
+    channel = var.release_channel
+  }
+
+  # Network configuration
+  network    = var.network
+  subnetwork = var.subnetwork
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = ""
+    services_secondary_range_name = ""
+  }
+
+  # Node pool configuration
   node_config {
     machine_type = var.machine_type
+    image_type   = var.image_type
+    disk_type    = var.disk_type
+    disk_size_gb = var.disk_size_gb
+
+    metadata = var.node_metadata
+
+    preemptible = var.spot
+
+    reservation_affinity {
+      consume_reservation_type = var.reservation_affinity
+    }
+
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
+
+    shielded_instance_config {
+      enable_integrity_monitoring = var.shielded_integrity_monitoring
+      enable_secure_boot         = var.shielded_secure_boot
+    }
   }
+
+  # Pod configuration
+  default_max_pods_per_node = var.default_max_pods_per_node
+
+  # Logging and monitoring
+  logging_service    = var.logging_service
+  monitoring_service = var.monitoring_service
+
+  # Addons
+  addons_config {
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+    http_load_balancing {
+      disabled = false
+    }
+    gce_persistent_disk_csi_driver_config {
+      enabled = true
+    }
+  }
+
+  # Security and monitoring features
+  binary_authorization {
+    evaluation_mode = var.binary_authorization_evaluation_mode
+  }
+
+  # Disable basic auth
+  master_auth {
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+
+  deletion_protection = false
 }
--- 01- GCP GKE Cluster/outputs.tf
+++ 01- GCP GKE Cluster/outputs.tf
@@ -3,17 +3,37 @@ output "cluster_name" {
   value       = google_container_cluster.primary.name
 }
 
+output "cluster_location" {
+  description = "Location of the cluster"
+  value       = google_container_cluster.primary.location
+}
+
 output "endpoint" {
   description = "Endpoint for the GKE cluster master"
   value       = google_container_cluster.primary.endpoint
 }
 
 output "master_version" {
-  description = "Kubernetes master version"
-  value       = google_container_cluster.primary.initial_cluster_version
+  description = "Current master version"
+  value       = google_container_cluster.primary.current_master_version
 }
 
 output "kubeconfig_command" {
-  description = "Command to configure kubectl credentials"
-  value       = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --zone ${var.zone} --project ${var.project_id}"
+  description = "Command to configure kubectl"
+  value       = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --region ${var.region} --project ${var.project_id}"
+}
+
+output "network" {
+  description = "Network used by the cluster"
+  value       = google_container_cluster.primary.network
+}
+
+output "subnetwork" {
+  description = "Subnetwork used by the cluster"
+  value       = google_container_cluster.primary.subnetwork
+}
+
+output "node_locations" {
+  description = "Node locations in the cluster"
+  value       = google_container_cluster.primary.node_locations
 }
